7fa98250124f80c1b5d3d153cf6a77da843c6e00
Guilherme Avelino <gavelino@gmail.com>
2020-12-31 16:56:04 -0300

    minor change 3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Aplicação de teste <code>ES2</code>  nEW2.
+          Aplicação de teste <code>ES2</code>  nEW - 3
         </p>
         <a
           className="App-link"
