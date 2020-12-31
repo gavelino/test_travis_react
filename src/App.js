79f56829782782c80a4135efac5fd8f1cd2e3612
Guilherme Avelino <gavelino@gmail.com>
2020-12-31 15:32:30 -0300

    add .travis.yml

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Aplicação de teste <code>ES2</code> .
         </p>
         <a
           className="App-link"
